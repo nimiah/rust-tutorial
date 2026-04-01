7016519aa87242e274df726effe83a83d1ce9cdd
chowcli <136085102+chowcli@users.noreply.github.com>
2026-04-01 18:45:41 +0700

    feat(article): define visibility update request and response models

--- advance_1/backend/src/models/article.rs
+++ advance_1/backend/src/models/article.rs
@@ -3,6 +3,18 @@ use serde::{Deserialize, Serialize};
 use sqlx::FromRow;
 use utoipa::ToSchema;
 
+#[derive(Deserialize, Debug, Clone, Serialize, ToSchema)]
+pub struct RequestUpdateArticleVisibility {
+    pub visibility: String,
+}
+
+#[derive(Deserialize, Debug, Clone, Serialize, ToSchema)]
+pub struct ArticleVisibilityResponse {
+    pub id: i32,
+    pub owner_id: i32,
+    pub visibility: String,
+}
+
 #[derive(Deserialize, Debug, Clone, Serialize, ToSchema, FromRow)]
 pub struct Article {
     pub id: i32,
@@ -14,4 +26,14 @@ pub struct Article {
     pub description: Option<String>,
     pub views: i64,
     pub likes: i64,
+}
+
+impl From<Article> for ArticleVisibilityResponse {
+    fn from(article: Article) -> Self {
+        Self {
+            id: article.id,
+            owner_id: article.owner_id,
+            visibility: article.visibility,
+        }
+    }
 }
